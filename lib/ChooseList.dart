a5ad498658032233e257c9b26d38e108112b41a7
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-01-16 08:04:58 -0500

    Added two classes to input items and choose shopping list

---
+++ lib/ChooseList.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ChooseList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Input Your Items!'),
+      ),
+      body: Text("Input Your Items"),
+    );
+  }
+}
---
+++ lib/InputItems.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class InputItems extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Select The Following Lists!'),
+      ),
+      body: Text("Select The Following Lists!"),
+    );
+  }
+}
