df28de2bf077a552d198852d1e5f65cd08d768d8
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-01-16 09:38:44 -0500

    Create a class for items

--- lib/InputItems.dart
+++ lib/InputItems.dart
@@ -1,5 +1,26 @@
 import 'package:flutter/material.dart';
 
+class Item{
+  String name;
+  int quantity;
+
+  Item({this.name, this.quantity});
+
+  static List<Item> getItems(){
+    return items;
+  }
+
+  static addItems(name, quantity){
+    var item = new Item();
+    item.name = name;
+    item.quantity = quantity;
+    items.add(item);
+  }
+}
+List<Item> items = [];
+
+//TODO: Implement the functionality for users to input their desired items.
+// Try using DataTable
 class InputItems extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
