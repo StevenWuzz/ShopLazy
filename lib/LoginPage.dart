e982430de4cb1ac7d8de267a5a55dae3828784f4
khalix <krutikshah01@gmail.com>
2021-01-16 09:52:47 -0500

    ligin authorization for real

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:shoplazy/SignUp.dart';
 import 'package:shoplazy/StartScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new _LoginPageState();
+  }
+}
+
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,72 +27,106 @@ class LoginPage extends StatelessWidget {
       ),
     );
   }
-}
 
-Widget _loginAndSignup(context) {
-  return Column(children: [
-    ButtonBar(
-      alignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        OutlinedButton(
-            onPressed: () => Navigator.push(
-                context, MaterialPageRoute(builder: (context) => SignUp())),
-            child: Text("Sign Up")),
-        ElevatedButton(
-            onPressed: () => Navigator.push(context,
-                MaterialPageRoute(builder: (context) => StartScreen())),
-            child: Text("Login")),
-      ],
-    ),
-    Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Checkbox(value: false, onChanged: null),
-        Text("Stay logged in?")
-      ],
-    )
-  ]);
-}
+  final formKey =
+      new GlobalKey<FormState>(); //I know global variables ar bad, but idc atm
+  String _email;
+  String _pass;
 
-Widget _usernameAndPasswords() { //TODO implement Firebase Auth
-  return Container(
-      margin: const EdgeInsets.all(20.0),
-      child: Column(
+  Widget _loginAndSignup(context) {
+    return Column(children: [
+      ButtonBar(
+        alignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Container(
-            margin: const EdgeInsets.only(bottom: 5.0),
-            child: TextField(
-              obscureText: false,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Email',
-              ),
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 5.0),
-            child: TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
-              ),
-            ),
-          ),
+          OutlinedButton(
+              onPressed: () => Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => SignUp())),
+              child: Text("Sign Up")),
+          ElevatedButton(
+              onPressed: () => validateAndSubmit(context),
+              child: Text("Login")),
         ],
-      ));
-}
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Checkbox(value: false, onChanged: null),
+          Text("Stay logged in?")
+        ],
+      )
+    ]);
+  }
 
-Widget _title() {
-  return Text(
-    "ShopLazy",
-    textAlign: TextAlign.center,
-    style: TextStyle(fontSize: 50),
-  );
-  /*return RichText(
+  bool validateAndSave() {
+    print("attempting to login");
+    final form = formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      print(_email + ":" + _pass);
+      return true;
+    }
+    return false;
+  }
+
+  Future<dynamic> validateAndSubmit(context) async {
+    if (validateAndSave()) {
+      try {
+        UserCredential userCredential = await FirebaseAuth.instance
+            .signInWithEmailAndPassword(email: _email, password: _pass);
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => StartScreen()));
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'user-not-found') {
+          print('No user found for that email.');
+        } else if (e.code == 'wrong-password') {
+          print('Wrong password provided for that user.');
+        } else {
+          print("Error: " + e.code);
+        }
+      }
+    }
+  }
+
+  Widget _usernameAndPasswords() {
+    return Container(
+        margin: const EdgeInsets.all(20.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              //margin: const EdgeInsets.only(bottom: 5.0),
+              new TextFormField(
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                  ),
+                  validator: (value) => value.isEmpty ? 'BOOO!!!' : null,
+                  onSaved: (value) => _email = value),
+              new TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                  ),
+                  validator: (value) => value.isEmpty ? 'BOOO!!!' : null,
+                  onSaved: (value) => _pass = value),
+            ],
+          ),
+        ));
+  }
+
+  Widget _title() {
+    return Text(
+      "ShopLazy",
+      textAlign: TextAlign.center,
+      style: TextStyle(fontSize: 50),
+    );
+    /*return RichText(
     textAlign: TextAlign.center,
     text: TextSpan(
       text: "ShopLazy",
     ),
   );*/
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:shoplazy/LoginPage.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ShopLazy',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: LoginPage(),
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return null;//SomethingWentWrong();
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'ShopLazy',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+            ),
+            home: LoginPage(),
+          );
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return null;//Loading();
+      },
     );
   }
 }
