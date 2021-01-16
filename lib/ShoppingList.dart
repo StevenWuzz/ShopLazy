4bfd0c22f98eaf1935911dc015dee7b2aaf94478
khalix <krutikshah01@gmail.com>
2021-01-16 15:44:07 -0500

    Making to andfrom json

--- lib/ShoppingList.dart
+++ lib/ShoppingList.dart
@@ -32,4 +32,35 @@ class ShoppingList {
   static Iterator<Item> getShoppingList() {
     return ShoppingList.shoppingList.iterator;
   }
+
+  factory ShoppingList.fromjson(Map<String,dynamic>) fromJson() => _ShoppngListFromJson(json);
+
+  Map<String,dynamic> toJson() => _ShoppingListToJson(this);
+
+}
+
+ShoppingList _ShoppingListFromJson(Map<String,dynamic> json) {
+  ShoppingList sl=new ShoppingList();
+  (json['Items']as List).forEach(item){
+    sl.add(Item.fromJson(item))
+  }
+}
+
+
+Map<String, dynamic> _ShoppingListToJson(ShoppingListToJson sl) => <String, dynamic> {
+  'user': sl.getname,
+  'notes': instance.notes,
+  'type': instance.type,
+  'vaccinations': _VaccinationList(instance.vaccinations),
+};
+// 4
+List<Map<String, dynamic>> _VaccinationList(List<Vaccination> vaccinations) {
+  if (vaccinations == null) {
+    return null;
+  }
+  List<Map<String, dynamic>> vaccinationMap =List<Map<String, dynamic>>();
+  vaccinations.forEach((vaccination) {
+    vaccinationMap.add(vaccination.toJson());
+  });
+  return vaccinationMap;
 }
