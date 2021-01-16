9f2c2c4979f2757ccde3d3c23dec4cf5a62f7654
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-01-16 07:46:09 -0500

    Added the two options for users

--- lib/StartScreen.dart
+++ lib/StartScreen.dart
@@ -12,25 +12,55 @@ class StartScreen extends StatelessWidget {
         child: Column(
           children: [
             _prompt(),
+            Options(),
           ],
         ),
       ),
     );
   }
 
+  // Asking the user to choose
   Widget _prompt() {
     return Text(
         "What Do You Want To Do?",
         style: TextStyle(fontSize: 30),
     );
   }
+}
 
-  //TODO: Use GridView
-  /*
-  Widget _options(){
-    return GridView.count(
-      crossAxisCount: 2,
-      children: Text(),
+// Two options for the user
+class Options extends StatelessWidget{
+  Options({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      height: 200,
+      child: GridView.count(
+        crossAxisCount: 2,
+        children: [
+          Card(
+            child: InkWell(
+              onTap: () {
+                //TODO: Transition to a new screen
+              },
+              child: Container(
+                child: Text('I Want To Shop'),
+              )
+            )
+          ),
+          Card(
+              child: InkWell(
+                  onTap: () {
+                    //TODO: Transition to a new screen
+                  },
+                  child: Container(
+                    child: Text('I Want To Ask Others To Shop For Me '),
+                  )
+              )
+          ),
+        ]
+      )
     );
-  }*/
+  }
 }
