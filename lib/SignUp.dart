b98376aba091a33f5c4312c223eae68891a974e5
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-01-16 09:23:12 -0500

    Completed barebone sign up page

--- lib/SignUp.dart
+++ lib/SignUp.dart
@@ -1,13 +1,51 @@
 import 'package:flutter/material.dart';
 
 class SignUp extends StatelessWidget {
-  @override //TODO Start and Finish
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('MainContent!'),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _usernameAndPasswords(),
+            ElevatedButton(
+                onPressed: () {
+                  //TODO: Store the data
+                },
+                child: Text("Sign Me Up!")),
+          ],
+        ),
       ),
-      body: Text("Helloooo World!"),
     );
   }
+}
+
+Widget _usernameAndPasswords() {
+  return Container(
+      margin: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(bottom: 5.0),
+            child: TextField(
+              obscureText: false,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Email',
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 5.0),
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+              ),
+            ),
+          ),
+        ],
+      ));
 }
