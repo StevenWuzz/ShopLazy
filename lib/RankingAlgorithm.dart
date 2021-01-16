e275c483536e07d2b583561fdf4cee5eff142a56
Vignesh Nanthakumar <vignesh@Vigneshs-MacBook-Pro.local>
2021-01-16 17:07:32 -0500

    Worked on algorithm

--- lib/RankingAlgorithm.dart
+++ lib/RankingAlgorithm.dart
@@ -1,5 +1,32 @@
 import "ShoppingList.dart";
+import "AdjacencyMatrix.dart";
 
 class Algorithm {
-  static void createAdjMat(List<ShoppingList> list) {}
+  static AdjacencyMatrix graph;
+
+  static List<ShoppingList> findMatch(ShoppingList list) {
+    Algorithm.graph = AdjacencyMatrix();
+
+    return traverseGraph(list.getUserEmail());
+  }
+
+  static List<ShoppingList> traverseGraph(String email) {
+    // Get Index of User Email
+    int index = graph.getEmailIndex(email);
+
+    // Get Rankings of all Users
+    List<int> rankings = graph.getAdjMat()[index];
+
+    rankings.sort();
+
+    List<int> ordered = new List.from(rankings.reversed);
+
+    List<ShoppingList> rankedLists;
+
+    for (int i = 0; i < ordered.length; i++) {
+      String listUser = graph.getEmail(ordered[i]);
+    }
+
+    return rankedLists;
+  }
 }
