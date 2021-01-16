b96319fd7637801175e02ef07f5c3647487c97e7
epicsteve2 <epicsteveoj@gmail.com>
2021-01-16 00:13:59 -0500

    made login screen nicer

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -19,36 +19,54 @@ class LoginPage extends StatelessWidget {
 }
 
 Widget _loginAndSignup(context) {
-  return ButtonBar(
-    alignment: MainAxisAlignment.spaceEvenly,
-    children: [
-      OutlinedButton(
-          onPressed: () => print("button1 Presseed!"), child: Text("Sign Up")),
-      ElevatedButton(
-          onPressed: () => print("button2 Presseed!"), child: Text("Login")),
-    ],
-  );
+  return Column(children: [
+    ButtonBar(
+      alignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        OutlinedButton(
+            onPressed: () => print("button1 Presseed!"),
+            child: Text("Sign Up")),
+        ElevatedButton(
+            onPressed: () => print("button2 Presseed!"), child: Text("Login")),
+      ],
+    ),
+    Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Checkbox(value: false, onChanged: null),
+        Text("Stay logged in?")
+      ],
+    )
+  ]);
 }
 
 Widget _usernameAndPasswords() {
-  return Column(
-    children: [
-      TextField(
-        obscureText: false,
-        decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: 'Email',
-        ),
-      ),
-      TextField(
-        obscureText: true,
-        decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: 'Password',
-        ),
-      ),
-    ],
-  );
+  return Container(
+      margin: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(bottom: 5.0),
+            child: TextField(
+              obscureText: false,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Email',
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 5.0),
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+              ),
+            ),
+          ),
+        ],
+      ));
 }
 
 Widget _title() {
