c4bb8d74d3f9514efece3ea70e9f429f47d4a474
Steven Hans Limantoro <40755531+StevenWuzz@users.noreply.github.com>
2021-01-16 20:17:26 -0500

    Fixed InputItems.dart logic and integrated with ShoppingList & Item classes

--- lib/InputItems.dart
+++ lib/InputItems.dart
@@ -1,27 +1,31 @@
 import 'package:flutter/material.dart';
 import 'ChooseList.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'ShoppingList.dart';
+import 'Item.dart';
 
 BuildContext testContext;
 
-class Item {
+class InputItem {
   String name;
   String quantity;
 
-  Item({this.name, this.quantity});
+  InputItem({this.name, this.quantity});
 
-  static List<Item> getItems() {
+  static List<InputItem> getItems() {
     return items;
   }
 
   static addItems(name, quantity) {
-    var item = new Item();
+    var item = new InputItem();
     item.name = name;
     item.quantity = quantity;
     items.add(item);
   }
 }
 
-List<Item> items = [];
+List<InputItem> items = [];
+var currentUser = ShoppingList(userEmail);
 
 class ShoppingTable extends StatefulWidget {
   ShoppingTable() : super();
@@ -31,17 +35,19 @@ class ShoppingTable extends StatefulWidget {
 }
 
 class ShoppingTableState extends State<ShoppingTable> {
-  List<Item> items;
-  List<Item> selectedItems;
+  List<InputItem> items;
+  List<InputItem> selectedItems;
+  bool pressed;
 
   @override
   void initState() {
     super.initState();
     selectedItems = [];
-    items = Item.getItems();
+    items = InputItem.getItems();
+    pressed = false;
   }
 
-  onSelectedItem(bool selected, Item item) async {
+  onSelectedItem(bool selected, InputItem item) async {
     setState() {
       if (selected == true) {
         selectedItems.add(item);
@@ -51,18 +57,20 @@ class ShoppingTableState extends State<ShoppingTable> {
     }
   }
 
+  /*
   deleteSelected() async {
     setState(() {
       if (selectedItems.isNotEmpty) {
-        List<Item> temp = [];
+        List<InputItem> temp = [];
         temp.addAll(selectedItems);
-        for (Item item in temp) {
+        for (InputItem item in temp) {
           items.remove(item);
           selectedItems.remove(item);
         }
       }
     });
   }
+  */
 
   SingleChildScrollView dataBody() {
     return SingleChildScrollView(
@@ -72,11 +80,12 @@ class ShoppingTableState extends State<ShoppingTable> {
           MaterialButton(
             child: Text('Add Another Item'),
             onPressed: () {
-              var item = new Item();
+              var item = new InputItem();
               item.name = "";
               item.quantity = "";
               items.add(item);
               setState(() {});
+              pressed = true;
             },
           ),
           DataTable(
@@ -92,27 +101,28 @@ class ShoppingTableState extends State<ShoppingTable> {
             rows: items
                 .map(
                   (item) => DataRow(
-                      selected: selectedItems.contains(item),
-                      onSelectChanged: (b) {
-                        onSelectedItem(b, item);
-                      },
-                      cells: [
-                        DataCell(
-                          TextField(
-                            onChanged: (text) {
-                              item.name = text;
-                            },
-                          ),
-                        ),
-                        DataCell(
-                          TextField(
-                            onChanged: (quantity){
-                              item.quantity = quantity;
-                            },
-                          )
-                        ),
-                      ]),
-                )
+                  selected: selectedItems.contains(item),
+                  onSelectChanged: (b) {
+                    onSelectedItem(b, item);
+                  },
+                  cells: [
+                    DataCell(
+                      TextField(
+                        onChanged: (text) {
+                          item.name = text;
+                        },
+                      ),
+                    ),
+                    DataCell(
+                        TextField(
+                          onChanged: (quantity){
+                            item.quantity = quantity;
+                            addToShoppingList(pressed, item.name, item.quantity);
+                          },
+                        )
+                    ),
+                  ]),
+            )
                 .toList(),
           ),
         ],
@@ -128,33 +138,24 @@ class ShoppingTableState extends State<ShoppingTable> {
         title: Text("Your Selected Items"),
       ),
       body: Column(
-        mainAxisSize: MainAxisSize.min,
+        //mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.center,
-        verticalDirection: VerticalDirection.down,
+        //verticalDirection: VerticalDirection.down,
         children: <Widget>[
           Expanded(
             child: dataBody(),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
+            //mainAxisSize: MainAxisSize.min,
             children: <Widget>[
               Padding(
                 padding: EdgeInsets.all(20.0),
                 child: OutlineButton(
-                  child: Text('SELECTED ${selectedItems.length}'),
-                  onPressed: () {},
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(20.0),
-                child: OutlineButton(
-                  child: Text('DELETE SELECTED'),
-                  onPressed: selectedItems.isEmpty
-                      ? null
-                      : () {
-                          deleteSelected();
-                        },
+                  child: Text('Checkout'),
+                  onPressed: () {
+                    //TODO: Store the data to the database and optionally transition
+                  },
                 ),
               ),
             ],
@@ -184,6 +185,24 @@ void onItemTap(int index) {
     Navigator.push(
         testContext, MaterialPageRoute(builder: (context) => ChooseList()));
   }
+}
+
+void addToShoppingList(bool pressed, String name, String quantity){
+  if(pressed == true){
+    int quant = int.tryParse(quantity);
+    if(quant != null){
+      var newItem = new Item(name, quant, "");
+      currentUser.addItem(newItem);
+    }
+    pressed = false;
+  }
+}
+
+final _auth = FirebaseAuth.instance;
+String userEmail;
+void getCurrentUserEmail() async {
+  final user = _auth.currentUser;
+  userEmail=user.email;
 }
 
 //TODO: Implement the functionality for users to input their desired items.
